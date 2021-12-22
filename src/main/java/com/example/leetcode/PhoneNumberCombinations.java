70fccf9bcedff199b6bd02dcbe7ede91e81da87d
Nick Ang <Nicholas_ang@outlook.com>
2021-12-22 23:54:33 +1100

    update

---
+++ src/main/java/com/example/leetcode/PhoneNumberCombinations.java
@@ -0,0 +1,10 @@
+package com.example.leetcode;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class PhoneNumberCombinations {
+    public static List<String> letterCombinations(String digits) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+    }
+}
---
+++ src/test/java/com/example/leetcode/PhoneNumberCombinationsTest.java
@@ -0,0 +1,33 @@
+package com.example.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class PhoneNumberCombinationsTest {
+
+    static Stream<Arguments> ArrayProvider() {
+        return Stream.of(
+                arguments("23", Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf")),
+                arguments("", List.of()),
+                arguments("2", Arrays.asList("a","b","c")
+        ));
+    }
+
+    @ParameterizedTest
+    @MethodSource({"ArrayProvider"})
+    void letterCombinations(String digits, List<String> expected) {
+        final List<String> result = PhoneNumberCombinations.letterCombinations(digits);
+        assertSoftly ((s) -> {
+            s.assertThat(result).isEqualTo(expected);
+        });
+    }
+}
