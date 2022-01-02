d96f6fabcfa6bff1871ae3318d20b35588c2dc66
Nick Ang <Nicholas_ang@outlook.com>
2022-01-02 23:05:50 +1100

    update

---
+++ src/test/java/com/example/leetcode/ZigZagConversionTest.java
@@ -0,0 +1,12 @@
+package com.example.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZigZagConversionTest {
+
+    @Test
+    void convert() {
+    }
+}
