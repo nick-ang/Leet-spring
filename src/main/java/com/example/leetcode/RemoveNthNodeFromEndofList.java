8af41a7ce94c4432392be6cd32282fd0649d62f2
Nick Ang <Nicholas_ang@outlook.com>
2022-01-14 00:24:12 +1100

    update

--- src/main/java/com/example/leetcode/RemoveNthNodeFromEndofList.java
+++ src/main/java/com/example/leetcode/RemoveNthNodeFromEndofList.java
@@ -1,8 +1,35 @@
 package com.example.leetcode;
+/*
+The entry point into a linked list is called the head of the list.
+It should be noted that head is not a separate node, but the
+reference to the first node.*/
+
+/*In order to solve this problem in only one pass and O(1) extra space,
+however, we would need to find a way to both reach the end of the list
+with one pointer and also reach the n'th node from the end simultaneously
+with a second pointer.*/
 
 public class RemoveNthNodeFromEndofList {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        return new ListNode(2, 4, 3);
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        // Set a fast and slow pointer at the head (start of list)
+        ListNode fast = head;
+        ListNode slow = head;
+        // loop fast to the nth node
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        // if the nth node is the last that means the list is only 1 long
+        if (fast == null) return head.next;
+        // use fast.next == null as exit condition and loop to end of list
+        while (fast.next != null) {
+            fast = fast.next;
+            // slow starts from the head while fast is already at the nth node
+            slow = slow.next;
+        }
+/*      once we hit fast.next == null i.e. end of the list, we know slow is at the node
+        before the end, and can use below to skip a node and return the list without the nth node*/
+        slow.next = slow.next.next;
+        return head;
     }
 }
 
--- src/test/java/com/example/leetcode/RemoveNthNodeFromEndofListTest.java
+++ src/test/java/com/example/leetcode/RemoveNthNodeFromEndofListTest.java
@@ -1,6 +1,10 @@
 package com.example.leetcode;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +12,20 @@ class RemoveNthNodeFromEndofListTest {
 
     @Test
     void removeNthFromEnd() {
-
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l4.next = l5;
+        ListNode listnode = RemoveNthNodeFromEndofList.removeNthFromEnd(l1,4 );
+        List<Integer> result = new ArrayList<Integer>();
+        while (listnode != null ) {
+            result.add(listnode.val);
+            if (listnode != null) listnode = listnode.next;
+        }
+        System.out.println(result);
     }
 }
