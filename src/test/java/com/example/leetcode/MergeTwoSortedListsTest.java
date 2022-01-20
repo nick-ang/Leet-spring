1b57589249043b8ca3f26d959f39d04bc64e4f72
Nick Ang <Nicholas_ang@outlook.com>
2022-01-20 23:53:54 +1100

    update

---
+++ src/test/java/com/example/leetcode/MergeTwoSortedListsTest.java
@@ -0,0 +1,12 @@
+package com.example.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeTwoSortedListsTest {
+
+    @Test
+    void mergeTwoLists() {
+    }
+}
